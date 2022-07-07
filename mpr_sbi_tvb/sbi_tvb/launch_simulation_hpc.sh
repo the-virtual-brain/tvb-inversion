3446b79f978acdbf9b02d06f1bab1f6f80945cf9
popaula937 <paula.popa@codemart.ro>
2022-07-07 10:57:19 +0300

    Prepare launch on HPC.

---
+++ mpr_sbi_tvb/sbi_tvb/launch_simulation_hpc.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+#echo "Pulling tvb-run:inv docker image"
+module load sarus && sarus pull thevirtualbrain/tvb-run:inv-jen
+
+start=$SECONDS
+
+echo "Start docker container"
+srun -C mc sarus --debug run --mount=type=bind,source=$PWD,destination=$1 thevirtualbrain/tvb-run:inv-jen /bin/bash -c "/opt/conda/envs/tvb-run/bin/python -m tvb.core.simulation_hpc_launcher $2"
+
+duration=$(( SECONDS - start ))
+
+echo "TVB operation run completed in $duration seconds"
