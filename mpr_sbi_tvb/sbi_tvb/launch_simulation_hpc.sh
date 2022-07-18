b907e7a3cc2660c2f2f705a07f29710e37acf94d
popaula937 <paula.popa@codemart.ro>
2022-07-18 16:19:00 +0300

    Typo

--- mpr_sbi_tvb/sbi_tvb/launch_simulation_hpc.sh
+++ mpr_sbi_tvb/sbi_tvb/launch_simulation_hpc.sh
@@ -6,7 +6,7 @@ module load sarus && sarus pull thevirtualbrain/tvb-run:inv-sbi
 start=$SECONDS
 
 echo "Start docker container"
-srun -C mc sarus --debug run --mount=type=bind,source=$PWD,destination=$1 thevirtualbrain/tvb-run:inv-jen /bin/bash -c "/opt/conda/envs/tvb-run/bin/python -m tvb.core.simulation_hpc_launcher $2"
+srun -C mc sarus --debug run --mount=type=bind,source=$PWD,destination=$1 thevirtualbrain/tvb-run:inv-sbi /bin/bash -c "/opt/conda/envs/tvb-run/bin/python -m tvb.core.simulation_hpc_launcher $2"
 
 duration=$(( SECONDS - start ))
 
