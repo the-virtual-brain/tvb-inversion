0988c556fea6cca25d0163829867546fdcae9f16
popaula937 <paula.popa@codemart.ro>
2022-07-20 10:47:06 +0300

    Use mem flag with slurm

--- mpr_sbi_tvb/sbi_tvb/launch_simulation_hpc.sh
+++ mpr_sbi_tvb/sbi_tvb/launch_simulation_hpc.sh
@@ -6,7 +6,7 @@ module load sarus && sarus pull thevirtualbrain/tvb-run:inv-sbi2
 start=$SECONDS
 
 echo "Start docker container"
-srun -C mc sarus --debug run --mount=type=bind,source=$PWD,destination=$1 thevirtualbrain/tvb-run:inv-sbi2 /bin/bash -c "/opt/conda/envs/tvb-run/bin/python -m tvb.core.simulation_hpc_launcher $2"
+srun -A ich012 -C mc --mem 32G sarus --debug run --mount=type=bind,source=$PWD,destination=$1 thevirtualbrain/tvb-run:inv-sbi2 /bin/bash -c "/opt/conda/envs/tvb-run/bin/python -m tvb.core.simulation_hpc_launcher $2"
 
 duration=$(( SECONDS - start ))
 
