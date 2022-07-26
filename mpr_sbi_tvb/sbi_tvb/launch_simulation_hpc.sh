0187b9c74377b93cddf294414d3eafb2589919a2
popaula937 <paula.popa@codemart.ro>
2022-07-26 18:12:39 +0300

    Review jenkins build.

--- mpr_sbi_tvb/sbi_tvb/launch_simulation_hpc.sh
+++ mpr_sbi_tvb/sbi_tvb/launch_simulation_hpc.sh
@@ -6,7 +6,7 @@ module load sarus && sarus pull thevirtualbrain/tvb-run:inv-sbi
 start=$SECONDS
 
 echo "Start sarus container"
-srun -C mc --mem 32G -n1 sarus --debug run --mount=type=bind,source=$PWD,destination=$1 thevirtualbrain/tvb-run:inv-sbi /bin/bash -c "/opt/conda/envs/tvb-run/bin/python -m tvb.core.simulation_hpc_launcher $2 $3 $4"
+srun -C mc --mem 32G -n1 sarus --debug run --mount=type=bind,source=$PWD,destination=$1 thevirtualbrain/tvb-run:inv-sbi /bin/bash -c "/opt/conda/envs/tvb-run/bin/python -m simulation_hpc_launcher $2 $3 $4"
 
 duration=$(( SECONDS - start ))
 
