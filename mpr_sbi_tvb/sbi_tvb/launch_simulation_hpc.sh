902aaedfa2249508da433b08396e8953be76cb1d
popaula937 <paula.popa@codemart.ro>
2022-08-29 13:01:13 +0300

    TVB-2958. Use full path for sarus as module command is not recognized anymore.

--- mpr_sbi_tvb/sbi_tvb/launch_simulation_hpc.sh
+++ mpr_sbi_tvb/sbi_tvb/launch_simulation_hpc.sh
@@ -1,12 +1,12 @@
 #!/bin/sh
 
 #echo "Pulling tvb-run:inv docker image"
-module load sarus && sarus pull thevirtualbrain/tvb-run:inv-sbi
+/apps/daint/system/opt/sarus/1.5.1/bin/sarus pull thevirtualbrain/tvb-run:inv-sbi
 
 start=$SECONDS
 
 echo "Start sarus container"
-sarus --debug run --mount=type=bind,source=$PWD,destination=$1 thevirtualbrain/tvb-run:inv-sbi /bin/bash -c "/opt/conda/envs/tvb-run/bin/python -m simulation_hpc_launcher $2 $3 $4"
+/apps/daint/system/opt/sarus/1.5.1/bin/sarus --debug run --mount=type=bind,source=$PWD,destination=$1 thevirtualbrain/tvb-run:inv-sbi /bin/bash -c "/opt/conda/envs/tvb-run/bin/python -m simulation_hpc_launcher $2 $3 $4"
 
 duration=$(( SECONDS - start ))
 
