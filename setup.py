b3a4f774d0240a7c97ecef2c56decfcef9182f8d
Lia Domide <ldomide@gmail.com>
2022-11-30 16:56:17 +0200

    mention GPL license
    Update version number to 0.4.0 in preparation for a future release

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 from setuptools import find_packages, setup
 
-TVB_INVERSION_VERSION = '0.3.0'
+TVB_INVERSION_VERSION = '0.4.0'
 SBI_TVB_REQUIREMENTS = ["autopep8", "sbi", "scipy", "torch", "numpy", "tvb-library", "dask", "distributed", "pandas"]
 TVB_INVERSION_TEAM = "Jan Fousek, Meysam Hashemi, Abolfazl Ziaee Mehr"
 
@@ -12,5 +12,6 @@ setup(
     author=TVB_INVERSION_TEAM,
     author_email='tvb.admin@thevirtualbrain.org',
     url='http://www.thevirtualbrain.org',
+    license="GPL-3.0-or-later",
     download_url='https://github.com/the-virtual-brain/tvb-inversion',
 )
